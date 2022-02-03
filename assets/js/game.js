6b09d368d9e2ec04f200940150c7a0f22481e3c3
Erica <erica.trenholm48@gmail.com>
2022-02-02 21:25:03 -0500

    add highscore feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -142,6 +142,23 @@ var randomNumber = function(min, max) {
   // function to end the entire game
   var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
+
+    // check localStorange for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    // if player has more money than the high score, player has a new high score
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has a high score of " + playerInfo.money + "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
   
     // if player is still alive, player wins!
     if (playerInfo.health > 0) {
@@ -150,6 +167,8 @@ var randomNumber = function(min, max) {
       window.alert("You've lost your robot in battle!");
     }
   
+
+
     // ask player if they'd like to play again
     var playAgainConfirm = window.confirm('Would you like to play again?');
   
